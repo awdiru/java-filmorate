561b8dbf6a8bb36a6840025eaf9c52e5eb9e71e7
awdiru <awdiru@yandex.ru>
2024-03-18 19:39:04 +0200

    убраны закомментированные участки

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.*;
 import ru.yandex.practicum.filmorate.annotation.films.realeseDate.AcceptableReleaseDate;
 
-//import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -17,17 +16,13 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class Film {
     private int id;
-    //
     @NotBlank(message = "Название фильма не должно быть пустым")
     private String name;
-    //
     @Size(max = 200, message = "Ошибка добавления фильма! Описание не может превышать 200 символов.")
     private String description;
-    //
     @NotNull(message = "Ошибка добавления фильма! Дата релиза не может быть пустой.")
     @AcceptableReleaseDate(message = "Ошибка добавления фильма! Дата релиза не может быть раньше 28 декабря 1895 года.")
     private LocalDate releaseDate;
-    //
     @NotNull(message = "Ошибка добавления фильма! Продолжительность не может быть пустой.")
     @Positive
     private int duration;
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -14,17 +14,13 @@ import java.time.LocalDate;
 public class User {
     @EqualsAndHashCode.Exclude
     private int id;
-    //
     @NotEmpty(message = "Ошибка добавления пользователя! email не может быть пустым.")
     @Email(message = "Ошибка добавления пользователя! email должен быть записан по форме 'user@email.org.'")
     private String email;
-    //
     @NotEmpty(message = "Ошибка добавления пользователя! Логин не может быть пустым.")
     @WithoutSpaces(message = "Ошибка добавления пользователя! Логин не может содержать пробелов.")
     private String login;
-    //
     private String name;
-    //
     @NotNull(message = "Ошибка добавления пользователя! Дата рождения не может быть пустой.")
     @Past(message = "Ошибка добавления пользователя! Дата рождения не может быть в будущем.")
     private LocalDate birthday;
